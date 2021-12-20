a2ea80ad2b00b9e2301871b0dee69a9a048e44dc
David <delvaldavid1@gmail.com>
2021-12-20 09:48:41 +0100

    Fix script on error
    Signed-off-by: David <delvaldavid1@gmail.com>

--- P5/check_tests.sh
+++ P5/check_tests.sh
@@ -25,10 +25,10 @@ for t in $(ls testSrc/test*.txt); do
   out_indeces='inde[0]="0"; inde[1]="0";'
   name_index=2
 
-  for t in $(seq $(( test_cases + 1 ))); do
-    out_files="$out_files name[$name_index]=\"in\"; name[$(( name_index + 1 ))]=\"out\";"
-    out_indeces="$out_indeces inde[$name_index]=\"$t\"; inde[$(( name_index + 1 ))]=\"$t\";"
-    name_index=$(( name_index + 2 ))
+  for t in $(seq $((test_cases + 1))); do
+    out_files="$out_files name[$name_index]=\"in\"; name[$((name_index + 1))]=\"out\";"
+    out_indeces="$out_indeces inde[$name_index]=\"$t\"; inde[$((name_index + 1))]=\"$t\";"
+    name_index=$((name_index + 2))
   done
   code="BEGIN{$out_indeces $out_files n=0}"'/----------?/{n++}{print >"test/test" number "-" name[n]  "-" inde[n] }'
   awk -v number="$i" "$code" testSrc/test"$i".txt
@@ -41,7 +41,7 @@ done
 printf "\e[34;4mRunning $test_count tests\e[39;0m\n"
 
 t_n=""
-for t in $(ls test/test*-entrada-0 ); do
+for t in $(ls test/test*-entrada-0); do
   i=$(echo "$t" | cut -d "-" -f 1 - | grep -o '[0-9]*')
   t_n=" $t_n $(echo -e "\n")$i"
 done
@@ -63,36 +63,37 @@ for i in $indeces; do
   err_dif=$(diff -Bb $error_file testOut/error$i)
   count_dif=$(echo $err_dif | wc -w)
   if [[ $((count_dif)) -ne 0 ]]; then
-      printf "\e[31;1mTest $i$test_name COMPILATION DIFF  \e[39;0m\n"
-      echo $err_dif
-      c_fail=$(( c_fail + 1 ))
+    printf "\e[31;1mTest $i$test_name COMPILATION DIFF  \e[39;0m\n"
+    echo $err_dif
+    c_fail=$((c_fail + 1))
   else
-      printf "\e[32;1mTest $i$test_name COMPILATION OK \e[39;0m\n"
-      c_ok=$(( c_ok + 1 ))
+    printf "\e[32;1mTest $i$test_name COMPILATION OK \e[39;0m\n"
+    c_ok=$((c_ok + 1))
   fi
 
+  if [[ -e "testBin/a${i}.out" ]]; then
+    for tcase in $(ls test/test${i}-in-*); do
+      n=$(echo "$tcase" | cut -d "-" -f 3 - | grep -o '[0-9]*')
+      out_file=$(echo $tcase | sed 's/in/out/g')
 
-  for tcase in $(ls test/test${i}-in-*); do
-    n=$(echo "$tcase" | cut -d "-" -f 3 - | grep -o '[0-9]*')
-    out_file=$(echo $tcase | sed 's/in/out/g')
+      out_t_file=$(echo $out_file | sed 's/test\//testOut\//g')
 
-    out_t_file=$(echo $out_file | sed 's/test\//testOut\//g')
+      "testBin/./a${i}.out" <$tcase 1>"$out_t_file"
 
-    "testBin/./a${i}.out" <$tcase 1>"$out_t_file"
+      out_dif=$(diff -Bb "$out_t_file" "$out_file")
+      count_dif=$(echo $out_dif | wc -w)
 
-    out_dif=$(diff -Bb "$out_t_file" "$out_file")
-    count_dif=$(echo $out_dif | wc -w)
-
-    if [[ $(($count_dif)) -eq 0 ]]; then
-      printf "\e[32;1mTest $i$test_name Case $n OK \e[39;0m\n"
-      r_ok=$(( r_ok + 1 ))
-    else
-      printf "\e[31;1mTest $i$test_name Case $n ERROR \e[39;0m\n"
-      r_fail=$(( r_fail + 1 ))
-      printf "\e[34;4mDifferences in the output\e[39;0m\n"
-      echo $out_dif
-    fi
-  done;
+      if [[ $(($count_dif)) -eq 0 ]]; then
+        printf "\e[32;1mTest $i$test_name Case $n OK \e[39;0m\n"
+        r_ok=$((r_ok + 1))
+      else
+        printf "\e[31;1mTest $i$test_name Case $n ERROR \e[39;0m\n"
+        r_fail=$((r_fail + 1))
+        printf "\e[34;4mDifferences in the output\e[39;0m\n"
+        echo $out_dif
+      fi
+    done
+  fi
   echo ""
 
 done
